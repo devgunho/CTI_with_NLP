b288dc57f1becd9102bf7cb0d3496941a0ec8de4
devgunho <devgun@kakao.com>
2021-03-26 18:26:22 +0900

    Add easygui

--- PYQT-FOR-NER/main.py
+++ PYQT-FOR-NER/main.py
@@ -1,7 +1,10 @@
 import sys
+
 from PyQt5 import uic
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
+
+import file_open_easygui as fopen
 
 form_class = uic.loadUiType("main_window.ui")[0]
 
@@ -15,6 +18,8 @@ class MyWindow(QMainWindow, form_class):
 
     def loadButtonFunction(self):
         print("LOAD Button Clicked.")
+        absPath = fopen.OpenWinFileExplorer()
+        print(absPath)
 
 
 if __name__ == "__main__":
